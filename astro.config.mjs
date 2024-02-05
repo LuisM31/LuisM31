66f092a045034562a3c1452538fef0e74416d416
Luis Miguel Montoya Henao <luism_999@hotmail.com>
2024-02-05 10:59:36 -0500

    Initial Structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://luism31.github.io',
-  base: '/my-repo',
-})
+  integrations: [tailwind()]
+});
